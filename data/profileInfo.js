19b95df7d9e8086ffcb53f6c34fd1892e3400add
Vu Quang <quangnv.0212@gmail.com>
2023-12-14 14:49:27 +0700

    u

--- data/profileInfo.js
+++ data/profileInfo.js
@@ -2,6 +2,6 @@ export const profileInfo = {
   name: "Vu Quang Nguyen",
   title: "Web Developer",
   imageSrcOne: "https://scontent.fhan2-3.fna.fbcdn.net/v/t39.30808-6/318844044_540025561507271_489876402112616100_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=174925&_nc_ohc=LmLGwuu2Jd0AX_rZfik&_nc_ht=scontent.fhan2-3.fna&oh=00_AfDKSQj_XTcaMPvy1H8D2WqKTwv-TEJtsVY6CXPpiB2NWQ&oe=64D91A45",
-  imageSrcTwo: "https://scontent-hkg4-1.xx.fbcdn.net/v/t39.30808-1/318844044_540025561507271_489876402112616100_n.jpg?stp=dst-jpg_p320x320&_nc_cat=101&ccb=1-7&_nc_sid=5f2048&_nc_ohc=DD3B6JXOtHoAX_uYSLt&_nc_ht=scontent-hkg4-1.xx&_nc_e2o=s&oh=00_AfDYvIYRyrzTvLG_nq4a0qL1WW0dLMImZ3ZyOuRDuLG4qw&oe=65366FE7",
+  imageSrcTwo: "https://scontent-hkg4-1.xx.fbcdn.net/v/t39.30808-6/318844044_540025561507271_489876402112616100_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=efb6e6&_nc_eui2=AeG51O6PYtqPnP0VAKUKBEQB-ETSBnPeLNv4RNIGc94s2-SC6BqiPNOeSm_BkOd7_nFRwrCWdZuwRYIjzfc_D59K&_nc_ohc=YBRebWOrC18AX_P1yvQ&_nc_ht=scontent-hkg4-1.xx&oh=00_AfCWxcgK_fYNIa3RIBWHCl0yu1ztvwmFNVncM5y85x7ahg&oe=657F3745",
   imageSrcThree: "https://scontent.fhan2-3.fna.fbcdn.net/v/t39.30808-6/318844044_540025561507271_489876402112616100_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=174925&_nc_ohc=LmLGwuu2Jd0AX_rZfik&_nc_ht=scontent.fhan2-3.fna&oh=00_AfDKSQj_XTcaMPvy1H8D2WqKTwv-TEJtsVY6CXPpiB2NWQ&oe=64D91A45",
 };
